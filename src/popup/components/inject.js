32dbc0fad5dd230be30ce1a59119e7b1b3f13f93
Rie Miyazaki <rie.miyazaki@axa.co.jp>
2018-04-10 20:33:01 +0900

    Implement to shadow default background if table row is selected.

--- src/popup/components/inject.js
+++ src/popup/components/inject.js
@@ -1,18 +1,23 @@
 (function ($) {
 
-    var $blockBackground = $('<div>', {id: 'blockBackground'});
-    var overlayCSS = {
-        backgroundColor: '#000',
-        opacity: 0.6,
-        width: '100%',
-        height: '100%',
-        top: '0px',
-        left: '0px',
-        position: 'fixed'
-    };
+    var blockBackground = $('body').find('#blockBackground');
 
-    $blockBackground.css(overlayCSS);
+    if(blockBackground.length === 0) {
+        var $blockBackground = $('<div>', {id: 'blockBackground'});
+        var overlayCSS = {
+            backgroundColor: '#000',
+            opacity: 0.6,
+            width: '100%',
+            height: '100%',
+            top: '0px',
+            left: '0px',
+            position: 'fixed'
+        };
 
-    $('body').append($blockBackground);
+        $blockBackground.css(overlayCSS);
+
+        $('body').append($blockBackground);
+    }
+
 
 })(jQuery);
