b1116f3c9fa8f90f8b600416eaccf92ba1f4b52a
Rie Miyazaki <rie.miyazaki@axa.co.jp>
2018-04-13 12:37:22 +0900

    WIP - Added hightlight

--- src/popup/components/inject.js
+++ src/popup/components/inject.js
@@ -14,6 +14,27 @@
             console.log("changed!! A " + data.selectedRow.selector);
             //chrome.storage.local.remove('selectedRow');
 
+            var position = $(data.selectedRow.selector).offset();
+
+            console.log("position : " + position.top + " " + position.left);
+
+            var testCSS =  {
+                position: 'absolute',
+                top: (position.top),
+                left: (position.left),
+                backgroundColor: '#000',
+                opacity: 0.6,
+                width : $(data.selectedRow.selector).width(),
+                height : $(data.selectedRow.selector).height(),
+            };
+
+
+            var div = $('<div id="new">');
+            div.appendTo($('body'));
+            div.css(testCSS);
+
+
+
         } else {
 
             $(data.selectedRow.selector).css({'background-color': '#FFFFFF'});
