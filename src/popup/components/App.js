53935b54f8422c67eb9699a0ace552e6e463b815
Rie MIyazaki <rie.miyazaki@axa.co.jp>
2018-05-28 22:12:39 +0900

    Implemented selectbox filter

--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -1,95 +1,127 @@
-/* eslint-disable no-trailing-spaces,padded-blocks,react/jsx-indent,indent */
+/* eslint-disable no-trailing-spaces,padded-blocks,react/jsx-indent,indent,keyword-spacing */
 import React from 'react'
-import {registerLanguage} from 'react-syntax-highlighter/dist/light'
+import { registerLanguage } from 'react-syntax-highlighter/dist/light'
 import js from 'react-syntax-highlighter/dist/languages/javascript'
 import styles from './App.css'
 import BootstrapTable from 'react-bootstrap-table-next'
+import filterFactory, { selectFilter } from 'react-bootstrap-table2-filter'
 
 registerLanguage('javascript', js)
 
-const tabs = ['Generate Code', 'Generate Test Case']
-
 const App = ({onSelectTab, selectedTab, onRestart, recording, components}) => {
 
-    const selectRowProp = {
+  const selectRowProp = {
 
-        mode: 'checkbox',
-        bgColor: 'pink', // you should give a bgcolor, otherwise, you can't regonize which row has been selected
-        hideSelectColumn: true, // enable hide selection column.
-        clickToSelect: true,  // you should enable clickToSelect, otherwise, you can't select column.
-        onSelect: (row, isSelected, e) => {
-            if (isSelected) {
+    mode: 'checkbox',
+    bgColor: 'pink', // you should give a bgcolor, otherwise, you can't regonize which row has been selected
+    hideSelectColumn: true, // enable hide selection column.
+    clickToSelect: true,  // you should enable clickToSelect, otherwise, you can't select column.
+    onSelect: (row, isSelected) => {
+      if (isSelected) {
 
-                row.isSelected = true
+        row.isSelected = true
 
-                chrome.storage.local.set({
-                    selectedRow: row
-                }, function () {
-                    chrome.tabs.executeScript(null, {file: './lib/jquery-1.10.2.min.js'}, function () {
-                        chrome.tabs.executeScript(null, {file: 'inject.js'})
-                    })
-                })
-            } else {
+        chrome.storage.local.set({
+          selectedRow: row
+        }, function () {
+          chrome.tabs.executeScript(null, {file: './lib/jquery-1.10.2.min.js'}, function () {
+            chrome.tabs.executeScript(null, {file: 'inject.js'})
+          })
+        })
+      } else {
 
-                row.isSelected = false
+        row.isSelected = false
 
-                chrome.storage.local.set({
-                    selectedRow: row
-                }, function () {
-                    chrome.tabs.executeScript(null, {file: './lib/jquery-1.10.2.min.js'}, function () {
-                        chrome.tabs.executeScript(null, {file: 'inject.js'})
-                    })
-                })
-            }
-        }
-
+        chrome.storage.local.set({
+          selectedRow: row
+        }, function () {
+          chrome.tabs.executeScript(null, {file: './lib/jquery-1.10.2.min.js'}, function () {
+            chrome.tabs.executeScript(null, {file: 'inject.js'})
+          })
+        })
+      }
     }
 
-    const columns = [{
-        dataField: 'id',
-        text: 'ID/SELECTOR',
-        classes: styles['col-id'],
-        headerStyle: {backgroundColor: '#49B882', color: 'white'},
-        headerClasses: styles['col-id']
+  }
 
-    }, {
-        dataField: 'name',
-        text: 'Name',
-        classes: styles['col-name'],
-        headerStyle: {backgroundColor: '#49B882', color: 'white'},
-        headerClasses: styles['col-name']
-    }, {
-        dataField: 'value',
-        text: 'Value',
-        classes: styles['col-value'],
-        headerStyle: {backgroundColor: '#49B882', color: 'white'},
-        headerClasses: styles['col-value']
-    }, {
-        dataField: 'tagName',
-        text: 'TagName',
-        classes: styles['col-tagName'],
-        headerStyle: {backgroundColor: '#49B882', color: 'white'},
-        headerClasses: styles['col-tagName']
-    }, {
-        dataField: 'inputType',
-        text: 'InputType',
-        classes: styles['col-tagName'],
-        headerStyle: {backgroundColor: '#49B882', color: 'white'},
-        headerClasses: styles['col-inputType']
-    }]
+  const selectOptions = {
+    'INPUT': 'INPUT',
+    'TEXTAREA': 'TEXTAREA'
+  }
 
-    return (<div>
+  const columns = [{
+    dataField: 'id',
+    text: 'ID/SELECTOR',
+    classes: styles['col-id'],
+    headerStyle: {backgroundColor: '#49B882', color: 'white'},
+    headerClasses: styles['col-id']
 
-        <div className={styles.tableWrapDiv}>
-            <BootstrapTable keyField='id' data={components} columns={columns} trClassName={styles.tableDiv} selectRow={selectRowProp} />
+  }, {
+    dataField: 'name',
+    text: 'Name',
+    classes: styles['col-name'],
+    headerStyle: {backgroundColor: '#49B882', color: 'white'},
+    headerClasses: styles['col-name']
+  }, {
+    dataField: 'value',
+    text: 'Value',
+    classes: styles['col-value'],
+    headerStyle: {backgroundColor: '#49B882', color: 'white'},
+    headerClasses: styles['col-value']
+  }, {
+    dataField: 'tagName',
+    text: 'TagName',
+    classes: styles['col-tagName'],
+    headerStyle: {backgroundColor: '#49B882', color: 'white'},
+    headerClasses: styles['col-tagName'],
+    formatter: cell => selectOptions[cell],
+    filter: selectFilter({
+      options: selectOptions,
+      defaultValue: ''
+    })
+  }, {
+    dataField: 'inputType',
+    text: 'InputType',
+    classes: styles['col-tagName'],
+    headerStyle: {backgroundColor: '#49B882', color: 'white'},
+    headerClasses: styles['col-inputType']
+  }]
 
-        </div>
+  const loadedDiv = (<div>
 
-        <div className={styles.buttonDiv}>
-            <button className={styles.button} onClick={onRestart}>Clear</button>
-        </div>
+    <div className={styles.tableWrapDiv}>
+      <BootstrapTable keyField='id' data={components} columns={columns} trClassName={styles.tableDiv}
+        selectRow={selectRowProp} filter={filterFactory()} />
 
-    </div>)
+    </div>
+
+    <div className={styles.buttonDiv}>
+      <button className={styles.button} onClick={onRestart}>Clear</button>
+    </div>
+
+  </div>)
+
+  const initDev = (<div>
+
+    Grats init
+
+    <div className={styles.buttonDiv}>
+      <button className={styles.button} onClick={onRestart}>Clear</button>
+    </div>
+
+  </div>)
+
+  console.log('test : ' + JSON.stringify(components))
+
+  if(components && components.length > 0) {
+    console.log('loaded')
+    return loadedDiv
+
+  } else {
+    console.log('init')
+    return initDev
+  }
+
 }
 
 export default App
--- src/popup/containers/App.js
+++ src/popup/containers/App.js
@@ -1,42 +1,41 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import App from '../components/App'
 
 export default class AppContainer extends Component {
-    constructor(props) {
-        super(props)
+  constructor (props) {
+    super(props)
 
-        this.state = {
-            recording: [],
-            components: []
-        }
-
-        this.onSelectTab = this.onSelectTab.bind(this)
-        this.onRestart = this.onRestart.bind(this)
+    this.state = {
+      recording: [],
+      components: []
     }
 
-    render() {
-        return React.createElement(App, {
-            ...this.props,
-            ...this.state,
-            onRestart: this.onRestart
-        })
-    }
+    this.onSelectTab = this.onSelectTab.bind(this)
+    this.onRestart = this.onRestart.bind(this)
+  }
 
-    componentDidMount() {
+  render () {
+    return React.createElement(App, {
+      ...this.props,
+      ...this.state,
+      onRestart: this.onRestart
+    })
+  }
 
-        chrome.storage.sync.get(['recording', 'components'], ({recording, components}) => {
-            this.setState({recording, components})
-        })
+// eslint-disable-next-line padded-blocks
+  componentDidMount () {
+    chrome.storage.sync.get(['recording', 'components'], ({recording, components}) => {
+      this.setState({recording, components})
+    })
+  }
 
-    }
+  onSelectTab (selectedTab) {
+    this.setState({selectedTab})
+  }
 
-    onSelectTab(selectedTab) {
-        this.setState({selectedTab})
-    }
-
-    onRestart() {
-        chrome.browserAction.setIcon({ path: './images/icon-black.png' })
-        chrome.runtime.reload()
-        window.close()
-    }
+  onRestart () {
+    chrome.browserAction.setIcon({path: './images/icon-black.png'})
+    chrome.runtime.reload()
+    window.close()
+  }
 }
