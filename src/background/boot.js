e99f46c8505eaf605a6df12702d02ec5bef642c7
Rie MIyazaki <rie.miyazaki@axa.co.jp>
2018-06-26 21:01:04 +0900

    Revert one click capture

--- src/background/boot.js
+++ src/background/boot.js
@@ -2,16 +2,27 @@ import Recorder from './recorder'
 
 export default class Boot {
   constructor () {
+    this.isRunning = false
     this.recorder = new Recorder()
   }
 
   boot () {
     // Fired when a browser action icon is clicked. This event will not fire if the browser action has a popup.
     chrome.browserAction.onClicked.addListener(() => {
-      this.recorder.start()
-      chrome.browserAction.setIcon({path: './images/icon-red.png'})
-      chrome.browserAction.setPopup({popup: 'index.html'})
-      this.recorder.stop()
+      if (this.isRunning) {
+        this.recorder.stop()
+        chrome.storage.sync.set({
+          tabId: this.recorder.tabId,
+          url: this.recorder.url,
+          components: this.recorder.components
+        })
+        chrome.browserAction.setIcon({path: './images/icon-red.png'})
+        chrome.browserAction.setPopup({popup: 'index.html'})
+      } else {
+        this.recorder.start()
+        chrome.browserAction.setIcon({path: './images/icon-green.png'})
+      }
+      this.isRunning = !this.isRunning
     })
 
     /*
