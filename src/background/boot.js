45d47a1b5de1939b77c4662535a8fdc6010a1709
Rie MIyazaki <rie.miyazaki@axa.co.jp>
2018-06-26 20:53:41 +0900

    Implement one click capture

--- src/background/boot.js
+++ src/background/boot.js
@@ -2,27 +2,16 @@ import Recorder from './recorder'
 
 export default class Boot {
   constructor () {
-    this.isRunning = false
     this.recorder = new Recorder()
   }
 
   boot () {
     // Fired when a browser action icon is clicked. This event will not fire if the browser action has a popup.
     chrome.browserAction.onClicked.addListener(() => {
-      if (this.isRunning) {
-        this.recorder.stop()
-        chrome.storage.sync.set({
-          tabId: this.recorder.tabId,
-          url: this.recorder.url,
-          components: this.recorder.components
-        })
-        chrome.browserAction.setIcon({path: './images/icon-red.png'})
-        chrome.browserAction.setPopup({popup: 'index.html'})
-      } else {
-        this.recorder.start()
-        chrome.browserAction.setIcon({path: './images/icon-green.png'})
-      }
-      this.isRunning = !this.isRunning
+      this.recorder.start()
+      chrome.browserAction.setIcon({path: './images/icon-red.png'})
+      chrome.browserAction.setPopup({popup: 'index.html'})
+      this.recorder.stop()
     })
 
     /*
