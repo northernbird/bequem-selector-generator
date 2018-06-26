46c07069099a29e98cabc8d074c325c16a1822c1
Rie MIyazaki <rie.miyazaki@axa.co.jp>
2018-06-26 20:20:31 +0900

    Fixed to focus submit buttons

--- src/popup/components/inject-insert.js
+++ src/popup/components/inject-insert.js
@@ -4,29 +4,9 @@
      */
   chrome.storage.local.get('selectedRow', function (data) {
     if (data.selectedRow.isSelected) {
-      console.log('selected : ' + data.selectedRow.selector)
       $(data.selectedRow.selector).toggleClass('bequem-selector-selected')
-
-      const position = $(data.selectedRow.selector).offset()
-      console.log('position : ' + position.top + ' ' + position.left)
-
-      // var testCSS = {
-      //   position: 'absolute',
-      //   top: (position.top),
-      //   left: (position.left),
-      //   backgroundColor: '#000',
-      //   opacity: 0.6,
-      //   width: $(data.selectedRow.selector).outerWidth(),
-      //   height: $(data.selectedRow.selector).outerHeight()
-      // }
-      //
-      // var div = $('<div id="new">')
-      // div.appendTo($('body'))
-      // div.css(testCSS)
     } else {
       $(data.selectedRow.selector).toggleClass('bequem-selector-selected')
-      // $(data.selectedRow.selector).css({'background-color': '#FFFFFF'})
-      console.log('changed!! B' + data.selectedRow.selector)
     }
   })
 })(jQuery)
