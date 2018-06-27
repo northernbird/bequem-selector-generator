9349567969e51bb6f84d7ffc155a89757e872086
Rie MIyazaki <rie.miyazaki@axa.co.jp>
2018-06-27 21:21:24 +0900

    Fixed to move ahead selector popup

--- src/popup/components/inject-insert.js
+++ src/popup/components/inject-insert.js
@@ -8,27 +8,50 @@
     if (data.selectedRow.isSelected) {
       $(data.selectedRow.selector).toggleClass('bequem-selector-selected')
 
-      const position = $(data.selectedRow.selector).offset()
+      const $currentElement = $(data.selectedRow.selector)
+      // const position = $currentElement.offset()
+      const injectCSS = {
+        position: 'absolute',
+        top: $(data.selectedRow.selector).outerHeight() + 5,
+        left: 0,
+        backgroundColor: '#FFF',
+        opacity: 0.9,
+        // width: $(data.selectedRow.selector).outerWidth() + 200,
+        // height: $(data.selectedRow.selector).outerHeight() + 300,
+        width: 180,
+        height: 30,
+        // overflow: 'visible',
+        // width: 0,
+        // height: 0,
+        'z-index': 1050,
+        'line-height': 3,
+        'font-size': '10px'
+      }
+      const createWrapper = () => {
+        const $wrapper = $('<div class="bequem-selector-wrapper">')
+        return $wrapper
+      }
+      const createContainer = () => {
+        const $container = $('<div id="' + injectId + '">')
+        $container.css(injectCSS)
+        $container.text(data.selectedRow.selector)
+        return $container
+      }
 
       if (data.selectedRow.inputType !== 'hidden') {
-        const injectCSS = {
-          position: 'absolute',
-          top: (position.top) + $(data.selectedRow.selector).outerHeight() + 5,
-          left: (position.left),
-          backgroundColor: '#FFF',
-          opacity: 0.9,
-          // width: $(data.selectedRow.selector).outerWidth() + 200,
-          // height: $(data.selectedRow.selector).outerHeight() + 300,
-          width: 240,
-          height: 30,
-          'z-index': 99999,
-          'font-size': '12px'
-        }
-
-        const div = $('<div id="' + injectId + '">')
-        div.appendTo($('body'))
-        div.css(injectCSS)
-        div.text(data.selectedRow.selector)
+        /*
+         * Get parent element
+         */
+        const $parent = $currentElement.parent()
+        $parent.css({position: 'static'})
+        /*
+         * Get wrapper element
+         */
+        const $wrapper = createWrapper()
+        const $container = createContainer()
+        $container.appendTo($wrapper)
+        $parent.prepend($wrapper)
+        // $wrapper.appendTo($parent)
       }
     } else {
       $(data.selectedRow.selector).toggleClass('bequem-selector-selected')
